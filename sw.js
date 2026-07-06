f1c718aaae6513372635d2fb28c4bb2241a2ff80
Surya <suryapratapvit2022@gmail.com>
2026-07-06 21:27:52 +0530

    Add PWA support: manifest, icons, service worker for offline/installable use

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "jargon-decoder-v1";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.add("./")));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
