ec8c9fd70f94b1f033dbefc2a28a30afa7dcb381
Surya <suryapratapvit2022@gmail.com>
2026-07-08 20:27:24 +0530

    Fix service worker caching stale content indefinitely; switch cache-first to network-first strategy

--- sw.js
+++ sw.js
@@ -1,19 +1,26 @@
-const CACHE = "jargon-decoder-v1";
+const CACHE = "jargon-decoder-v2";
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then((cache) => cache.add("./")));
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
